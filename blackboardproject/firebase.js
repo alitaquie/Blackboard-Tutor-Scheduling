6d0010a9c51720f8cc054551f68730380b2d4d4d
Brian <bbbae@ucsc.edu>
2024-04-12 19:13:40 -0700

    html for login

---
+++ blackboardproject/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAPWJQdZePfzjpRlRtMT_gY3HktQvqizAw",
+  authDomain: "blackboard-41f8a.firebaseapp.com",
+  projectId: "blackboard-41f8a",
+  storageBucket: "blackboard-41f8a.appspot.com",
+  messagingSenderId: "52140658788",
+  appId: "1:52140658788:web:46a71eb9e5a26e86ce0f71",
+  measurementId: "G-W7BR765CXM"
+};
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length == 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+    app = firebase.app()
+}
+const auth = firebase.auth()
+
+export { auth };
