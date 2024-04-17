6d478ce42b6d4425005fa02c908b5935089577bf
Mohammad Taquie <alitaquie51@gmail.com>
2024-04-17 16:30:28 -0700

    created navbar

--- Blackboard/screens/HomeScreen.js
+++ Blackboard/screens/HomeScreen.js
@@ -1,11 +1,25 @@
 import { ImageBackground, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native'
 import React from 'react'
+import Icon from 'react-native-vector-icons/Ionicons'
+import Icon2 from 'react-native-vector-icons/FontAwesome5'
 
 const HomeScreen = () => {
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
-      <View>
+      <View style={styles.content}>
         <Text>Home Screen</Text>
+      </View>
+      <View style={styles.navbar}>
+        <TouchableOpacity style={styles.navItem}>
+        <Icon2 name="home" size={30} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navItem}>
+        <Icon2 name="book" size={30} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navItem}>
+        <Icon name="person-circle" size={30} color="white" />
+        </TouchableOpacity>
+        {/* Add more items as needed */}
       </View>
     </KeyboardAvoidingView>
   )
@@ -17,6 +31,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black'
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center'
-},
+  },
+  navbar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    paddingVertical: 35,
+    paddingHorizontal: 20,
+    width: '100%',
+    borderTopWidth: 1,
+    borderTopColor: 'white'
+  },
+  navItem: {
+    alignItems: 'center'
+  }
 })
