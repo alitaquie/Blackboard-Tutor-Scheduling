679b35424f443b47458d3c273dc3fd20c9fad95e
Bruce Bai <brbai@ucsc.edu>
2024-04-19 09:35:46 -0700

    pulled navbar

--- Blackboard/screens/HomeScreen.js
+++ Blackboard/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import { ImageBackground, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native'
 import React from 'react'
+import Navbar from 'Navbar.js'
 
 const HomeScreen = () => {
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View>
         <Text>Home Screen</Text>
+        <Navbar></Navbar>
       </View>
     </KeyboardAvoidingView>
   )
