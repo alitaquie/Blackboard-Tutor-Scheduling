0986a4ad868d4d970a67eefc927ff9ba2466ed1f
Mohammad Taquie <alitaquie51@gmail.com>
2024-04-26 09:25:11 -0700

    add class button added

--- Blackboard/screens/AddClass.js
+++ Blackboard/screens/AddClass.js
@@ -1,9 +1,9 @@
-import { ImageBackground, KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Icon from 'react-native-vector-icons/Ionicons'
-import Icon2 from 'react-native-vector-icons/FontAwesome5'
+import React from 'react';
+import { View, Text, StyleSheet, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Icon2 from 'react-native-vector-icons/FontAwesome5';
 import { useNavigation } from '@react-navigation/native';
-import Navbar from './NavBar';
+import Navbar from './Navbar';
 
 const ClassScreen = () => {
   const navigation = useNavigation();
@@ -12,25 +12,41 @@ const ClassScreen = () => {
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.content}>
         <Text>Class Screen</Text>
+        {/* Button added to the middle of the screen */}
+        <TouchableOpacity style={styles.button} onPress={() => {/* Handle button press */}}>
+          <Text style={styles.buttonText}>Add Class</Text>
+        </TouchableOpacity>
       </View>
-        <Navbar navigation={navigation} />
+      <Navbar navigation={navigation} />
     </KeyboardAvoidingView>
   );
 };
 
-export default ClassScreen
+export default ClassScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'red'
+    backgroundColor: 'cyan',
   },
   content: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: 'blue',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
   navbar: {
     flexDirection: 'row',
@@ -41,9 +57,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     width: '100%',
     borderTopWidth: 1,
-    borderTopColor: 'white'
+    borderTopColor: 'white',
   },
   navItem: {
-    alignItems: 'center'
-  }
-})
+    alignItems: 'center',
+  },
+});
